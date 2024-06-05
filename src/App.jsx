b20c3bb9b6fef9b997aa18fd66e54bfaeaac5a41
Carlos Eduardo <carloseduardosilva5858@gmail.com>
2024-06-05 19:47:37 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 import 'bootstrap/dist/js/bootstrap.min.js';
 import Home from './pages/home/home';
-import Header from './components/Header/header';
 
 function App() {
 
   return (
     <>
-     <Header />
       <Home/>
     </>
   )
